b6d17c5bdc39829a0ce48223b447da9f4d817dd7
mr. Vovok <mr. Vovok@DESKTOP-VUCH0C5>
2025-03-31 22:41:37 +0300

    Добавьте файлы проекта.

---
+++ Expression.cpp
@@ -0,0 +1,148 @@
+﻿// Expression.cpp : Определяет функции для статической библиотеки.
+//
+
+#include "pch.h"
+#include "framework.h"
+#include "Expression.h"
+
+//основные функции
+double Expression::solve() {
+	for (int i = 0; i < expressionVec.size() - 1; i++) {
+		if (expressionVec[i] == "*") {
+			string answer = to_string(stod(expressionVec[i - 1]) * stod(expressionVec[i + 1]));
+			auto it = expressionVec.begin() + (i - 1);
+			expressionVec.erase(it, it + 3);
+			expressionVec.insert(expressionVec.begin() + (i - 1), answer);
+			solve();
+		}
+		else if (expressionVec[i] == "/") {
+			string answer = to_string(stod(expressionVec[i - 1]) / stod(expressionVec[i + 1]));
+			auto it = expressionVec.begin() + (i - 1);
+			expressionVec.erase(it, it + 3);
+			expressionVec.insert(expressionVec.begin() + (i - 1), answer);
+			solve();
+		}
+	}
+	for (int i = 0; i < expressionVec.size() - 1; i++) {
+		if (expressionVec[i] == "+") {
+			string answer = to_string(stod(expressionVec[i - 1]) + stod(expressionVec[i + 1]));
+			auto it = expressionVec.begin() + (i - 1);
+			expressionVec.erase(it, it + 3);
+			expressionVec.insert(expressionVec.begin() + (i - 1), answer);
+			solve();
+		}
+		else if (expressionVec[i] == "-") {
+			string answer = to_string(stod(expressionVec[i - 1]) - stod(expressionVec[i + 1]));
+			auto it = expressionVec.begin() + (i - 1);
+			expressionVec.erase(it, it + 3);
+			expressionVec.insert(expressionVec.begin() + (i - 1), answer);
+			solve();
+		}
+	}
+	return stod(expressionVec[0]);
+}
+Expression Expression::multiply(double num) {
+	for_each(expressionVec.begin(), expressionVec.end(),
+		[num, this](string& element) {
+			if (is_number(element)) element = to_string(stod(element) * num); 
+			element.erase(element.find_last_not_of('0') + 1, string::npos); //чистка нулей
+			element.erase(element.find_last_not_of('.') + 1, string::npos); //
+		});
+	return *this;
+}
+Expression Expression::divide(double num) {
+	for_each(expressionVec.begin(), expressionVec.end(),
+		[num, this](string& element) {
+			if (is_number(element)) element = to_string(stod(element) / num);
+			element.erase(element.find_last_not_of('0') + 1, string::npos); //чистка нулей
+			element.erase(element.find_last_not_of('.') + 1, string::npos); //
+		});
+	return *this;
+}
+Expression Expression::subtract(double num) {
+	for_each(expressionVec.begin(), expressionVec.end(),
+		[num, this](string& element) {
+			if (is_number(element)) element = to_string(stod(element) - num);
+			element.erase(element.find_last_not_of('0') + 1, string::npos); //чистка нулей
+			element.erase(element.find_last_not_of('.') + 1, string::npos); //
+		});
+	return *this;
+}
+Expression Expression::add(double num) {
+	for_each(expressionVec.begin(), expressionVec.end(),
+		[num, this](string& element) {
+			if (is_number(element)) element = to_string(stod(element) + num);
+			element.erase(element.find_last_not_of('0') + 1, string::npos); //чистка нулей
+			element.erase(element.find_last_not_of('.') + 1, string::npos); //
+		});
+	return *this;
+}
+
+//сеттеры
+void Expression::set_expr(string exprStr) {
+	expressionStr = exprStr;
+	string tmpExprStr;
+	for (int i = 0; i <= expressionStr.length(); i++) {
+		if (isdigit(expressionStr[i]) || expressionStr[i] == '.')
+			tmpExprStr += expressionStr[i];
+		else if (expressionStr[i] == '(') {
+			string underExprStr;
+			while (expressionStr[i + 1] != ')') {
+				underExprStr += expressionStr[++i];
+				if (i + 1 == expressionStr.length()) expressionStr += ')';
+			}
+			brackets.push_back(underExprStr);
+			Expression expression(underExprStr);
+			string ans = to_string(expression.solve());
+			ans.erase(ans.find_last_not_of('0') + 1, string::npos); //чистка нулей
+			ans.erase(ans.find_last_not_of('.') + 1, string::npos); //
+			expressionVec.push_back(ans);
+		}
+		else {
+			expressionVec.push_back(tmpExprStr);
+			tmpExprStr = expressionStr[i];
+			expressionVec.push_back(tmpExprStr);
+			tmpExprStr = "";
+		}
+	}
+	clean_vector();
+}
+
+//внутренние методы
+void Expression::clean_vector() {
+	auto deleted = remove_if(expressionVec.begin(), expressionVec.end(),
+		[this](string element) {
+			if (!is_number(element) && !is_operator(element)) return true;
+			return false;
+		});
+	expressionVec.erase(deleted, expressionVec.end());
+
+}
+bool Expression::is_operator(string symbol) {
+	const char* operators = new char[4] {'*', '/', '+', '-'};
+	if (symbol.length() != 1) return false;
+	for (int j = 0; j < 4; j++) {
+		if (to_char(symbol) == operators[j]) return true;
+	}
+	return false;
+}
+bool Expression::is_number(string symbol) {
+	try {
+		if (stod(symbol)) return true;
+		return false;
+	}
+	catch (...) {
+		return false;
+	}
+}
+char Expression::to_char(string symbol) {
+	try {
+		if (symbol.length() != 1) throw "ti ebanat???";
+		return symbol.c_str()[0];
+	}
+	catch (const char* message) {
+		cout << message;
+		return 0;
+	}
+}
+
---
+++ Expression.h
@@ -0,0 +1,72 @@
+#pragma once
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+class Expression
+{
+public:
+	//конструкторы
+	Expression(string expressionStr) : expressionStr(expressionStr)
+	{
+		set_expr(expressionStr);
+	}
+	Expression() : expressionStr(""), expressionVec(NULL) {}
+
+	//основные функции
+	double solve();
+	Expression multiply(double num);
+	Expression divide(double num);
+	Expression subtract(double num);
+	Expression add(double num);
+
+	//операторы
+	bool operator== (Expression expr) {
+		return solve() == expr.solve();
+	}
+	bool operator!= (Expression expr) { 
+		return solve() != expr.solve();
+	}
+
+	//сеттеры
+	void set_expr(string exprStr);
+	void set_value(int index, string val) { expressionVec[index] = val; }
+
+	//геттеры
+	int size()                    { return expressionVec.size(); }
+	string at(int index)          { return expressionVec[index]; }
+	string get_string()           { return expressionStr; }
+	vector<string> get_vector()   { return expressionVec; }
+	vector<string> get_brackets() { return brackets; }
+
+	//принты
+	void print_vector() {
+		for (string el : expressionVec) {
+			printf("[%s] ", el.c_str());
+		}
+		cout << "\n";
+	}
+	void print_brackets() {
+		for_each(brackets.begin(), brackets.end(),
+			[](string content) {cout << "(" << content << ") "; });
+		cout << "\n";
+	}
+	void print_string() {
+		cout << expressionStr << "\n";
+	}
+
+private:
+	string expressionStr;         //строковая запись выражения
+	vector<string> expressionVec; //разбитое на части выражение
+	vector<string> brackets;      //содержимое в скобках
+
+	//внутренние методы
+	void clean_vector();
+	bool is_operator(string symbol);
+	bool is_number(string symbol);
+	char to_char(string symbol);
+};
